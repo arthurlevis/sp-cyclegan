aa5ebb78f856008049d05522d1fa6debc89b8428
Arthur LEVISALLES <alevisal@beaker.cs.ucl.ac.uk>
2025-05-09 15:31:00 +0100

    Command to run scripts on cluster

--- test.sh
+++ test.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+#$ -N job_name
+#$ -l tscratch=<n>G
+#$ -l tmem=<n>G
+#$ -l gpu=true
+#$ -q gpu.q
+
+# Create scratch directory
+mkdir -p /scratch0/username/${JOB_ID}
+
+# Copy test data to scratch
+cp -r path/to/test_data /scratch0/username/${JOB_ID}/
+
+# Cleanup function
+function finish {
+  rm -rf /scratch0/username/${JOB_ID}
+}
+trap finish EXIT ERR INT TERM
+
+# Run test script on cluster
 python test.py \
-	--dataroot ./pilot_dataset \
-	--checkpoints_dir . \
-	--name pretrained \
-	--epoch 30 \
-	--model cycle_gan_struct \
-	--gpu_ids -1 \
-	--num_test 21
+	--dataroot /scratch0/username/${JOB_ID}/data \
--- train.sh
+++ train.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-python train.py \
-	--dataroot ./pilot_dataset \
-	--name pilot_experiment \
-	--model cycle_gan_struct \
-	--lambda_struct 1.0 \
-	--gpu_ids -1 \
-	--batch_size 4 \
-	--n_epochs 1 \
-	--n_epochs_decay 1 \
-	--save_epoch_freq 1
+#$ -N job_name
+#$ -l tscratch=<n>G
+#$ -l tmem=<n>G
+#$ -l gpu=true
+#$ -q gpu.q
+
+# Create scratch directory
+mkdir -p /scratch0/username/${JOB_ID}
+
+# Copy training data to scratch
+cp -r path/to/training_data /scratch0/username/${JOB_ID}/
+
+# Cleanup function
+function finish {
+  rm -rf /scratch0/username/${JOB_ID}
+}
+trap finish EXIT ERR INT TERM
+
+# Run training script on cluster
+python train.py \ 
+	--dataroot /scratch0/username/${JOB_ID}/data \ 
+	--name experiment_name \ 
+	--model cycle_gan_struct \ 
+	--lambda_struct 1.0        # added
+
