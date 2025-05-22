680ffdd12dff56908a8604057c78ceb590c2bacf
arthurlevis <arthur.levisalles10@gmail.com>
2025-05-22 14:31:33 +0100

    .

--- datasets/prepare_dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run training script on cluster
-python prepare_dataset.py \
-    --dataset_path /SAN/medic/RealCol/exp1_dataset \
-    --synthetic1 /SAN/medic/RealCol/datasets_zip/SyntheticColon_II.zip \
-    --synthetic2 /SAN/medic/RealCol/datasets_zip/SyntheticColon_III.zip \
-    --real /SAN/medic/RealCol/datasets_zip/oblique_train.zip 
- 
