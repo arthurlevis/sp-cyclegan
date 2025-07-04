9030206e0894a418c7dc8f07f3d2a77ab3411cef
Arthur LEVISALLES <alevisal@beaker.cs.ucl.ac.uk>
2025-07-04 10:06:19 +0100

    added temporal and optical flow losses

--- models/cycle_gan_struct_model.py
+++ models/cycle_gan_struct_model.py
@@ -5,12 +5,20 @@ import itertools
 from util.image_pool import ImagePool
 from .base_model import BaseModel
 from . import networks
+from .mutual_information import MIScore
 try:
     from apex import amp
 except ImportError as error:
     print(error)
+# # Arthur: import PyTorch's AMP as an alternative to apex (AMP benefits come with larger batch sizes)
+# from torch.amp import GradScaler, autocast
+# Arthur: import RAFT
+from torchvision.models.optical_flow import raft_large
+from torchvision.models.optical_flow import Raft_Large_Weights
+from torchvision.utils import flow_to_image
+# Arthur: import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 
-from .mutual_information import MIScore
 
 class CycleGANStructModel(BaseModel):
     """
@@ -49,10 +57,14 @@ class CycleGANStructModel(BaseModel):
             parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
             parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
             parser.add_argument('--lambda_identity', type=float, default=0.5, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
-
-            # shuxian: additional args for structural loss
+            # shuxian: additional args for structural loss & depth estimation
             parser.add_argument('--lambda_struct', type=float, default=1.0, help='weight for structural loss (depth consistency)')
             # parser.add_argument('--tgt_depth', type=str, default='', help='model for depth esimtation in domain B')
+            # Arthur: additional args for temporal loss & optical flow loss
+            parser.add_argument('--lambda_temporal', type=float, default=0.0, help='weight for temporal loss (temporal consistency)')
+            parser.add_argument('--lambda_flow', type=float, default=0.0, help='weight for optical flow consistency loss')
+            # # Arthur: add mixed precision training option
+            # parser.add_argument('--amp', action='store_true', help='use automatic mixed precision')
 
         return parser
 
@@ -68,20 +80,32 @@ class CycleGANStructModel(BaseModel):
         self.opt.amp = False
 
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B', 'struct']
+        # self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B', 'struct']
+        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B', 'struct', 'temporal', 'flow']
+
+        # initialize previous frames
+        self.prev_real_A = None
+        self.prev_fake_B = None
+
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
-        # visual_names_A = ['real_A', 'fake_B', 'rec_A']
-        # visual_names_B = ['real_B', 'fake_A', 'rec_B'] 
-        # Only save fake_B (Arthur Levisalles)
-        visual_names_A = ['fake_B']  # consider to add 'rec_A' for evaluation
+        # visual_names_A = ['real_A', 'fake_B', 'rec_A']  # AtoB translation
+        # visual_names_B = ['real_B', 'fake_A', 'rec_B']  # BtoA translation
+        visual_names_A = ['fake_B']
         visual_names_B = [] 
-        if self.isTrain and self.opt.lambda_identity > 0.0:  # if identity loss is used, we also visualize idt_B=G_A(B) ad idt_A=G_A(B)
-            visual_names_A.append('idt_B')
-            visual_names_B.append('idt_A')
+
+        # if self.isTrain and self.opt.lambda_identity > 0.0:  # if identity loss is used, we also visualize idt_B=G_A(B) & idt_A=G_A(B)
+        #     visual_names_A.append('idt_B')
+        #     visual_names_B.append('idt_A')
+
         # # shuxian: add estimated depth visualization
         # visual_names_A.append('fake_B_depth')
 
-        self.visual_names = visual_names_A + visual_names_B  # combine visualizations for A and B
+        # Arthur: add optical flow images
+        if self.isTrain and self.opt.lambda_flow > 0:
+            visual_names_A.extend(['flow_real_A_img', 'flow_fake_B_img'])
+
+        self.visual_names = visual_names_A + visual_names_B  # combine visualizations for A & B
+        
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>.
         if self.isTrain:
             self.model_names = ['G_A', 'G_B', 'D_A', 'D_B']
@@ -119,6 +143,43 @@ class CycleGANStructModel(BaseModel):
 
             self.struct_loss = MIScore(bins=256,min=0,max=1)
 
+            # # Arthur: mixed precision training
+            # if self.opt.amp:
+            #     self.scaler = GradScaler()
+        
+        # Arthur: RAFT preprocessing
+        weights = Raft_Large_Weights.DEFAULT
+        self.raft_transforms = weights.transforms()
+        
+        # Arthur: create RAFT model (for optical flow)
+        self.raft_model = raft_large(weights=None).to(self.device)
+
+        # Load weights (download weights with "wget https://download.pytorch.org/models/raft_large_C_T_SKHT_V2-ff5fadd5.pth -O raft_weights.pth")
+        state_dict = torch.load('RAFT/raft_weights.pth', map_location=self.device)
+        self.raft_model.load_state_dict(state_dict)
+        self.raft_model.eval()
+
+        for param in self.raft_model.parameters():
+            param.requires_grad = False  # freeze RAFT
+        
+        # Arthur: add tensorboard logging
+        if self.isTrain:
+            self.writer = SummaryWriter(f'checkpoints/{opt.name}')
+            self.step = 0
+
+    def compute_raft_flow(self, frame1, frame2):
+        """Arthur: compute optical flow using the RAFT model.
+
+        Parameters:
+            frame1, frame2: tensors with shape [B, 3, H, W] where B = batch size & 3 = RGB channels.
+        """
+        frame1_01 = (frame1 + 1) / 2
+        frame2_01 = (frame2 + 1) / 2
+        frame1_raft, frame2_raft = self.raft_transforms(frame1_01, frame2_01)
+        with torch.no_grad():
+            flow = self.raft_model(frame1_raft, frame2_raft)[-1]
+        return flow
+
     def set_input(self, input):
         """Unpack input data from the dataloader and perform necessary pre-processing steps.
 
@@ -138,19 +199,30 @@ class CycleGANStructModel(BaseModel):
         # else:
         #     self.real_A_depth = None
 
-        # Only load depths during training (Arthur Levisalles)
-        # if self.isTrain and AtoB and 'A_depth' in input:
+        # Arthur: only load depths during training (saves memory & faster)
         if self.isTrain and AtoB:
             self.real_A_depth = input['A_depth'].to(self.device)
         else:
             self.real_A_depth = None
 
     def forward(self):
-        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        """Run forward pass; called by both functions <optimize_parameters> and <test>."""  # generates outputs
         self.fake_B = self.netG_A(self.real_A)  # G_A(A)
         self.rec_A = self.netG_B(self.fake_B)   # G_B(G_A(A))
         self.fake_A = self.netG_B(self.real_B)  # G_B(B)
         self.rec_B = self.netG_A(self.fake_A)   # G_A(G_B(B))
+        # # Arthur: mixed precision wrapper
+        # if self.opt.amp:
+        #     with autocast('cuda'):
+        #         self.fake_B = self.netG_A(self.real_A)
+        #         self.rec_A = self.netG_B(self.fake_B)
+        #         self.fake_A = self.netG_B(self.real_B)
+        #         self.rec_B = self.netG_A(self.fake_A)
+        # else:
+        #     self.fake_B = self.netG_A(self.real_A)
+        #     self.rec_A = self.netG_B(self.fake_B)
+        #     self.fake_A = self.netG_B(self.real_B)
+        #     self.rec_B = self.netG_A(self.fake_A)
 
     def backward_D_basic(self, netD, real, fake):
         """Calculate GAN loss for the discriminator
@@ -169,13 +241,29 @@ class CycleGANStructModel(BaseModel):
         # Fake
         pred_fake = netD(fake.detach())
         loss_D_fake = self.criterionGAN(pred_fake, False)
-        # Combined loss and calculate gradients
+        # Combined loss & calculate gradients
         loss_D = (loss_D_real + loss_D_fake) * 0.5
         if self.opt.amp:
             with amp.scale_loss(loss_D, self.optimizer_D) as scaled_loss:
                 scaled_loss.backward()
         else:
             loss_D.backward()
+        # # Arthur: mixed precision wrapper (move loss calculations inside autocast to maintain consistent dtypes)
+        # if self.opt.amp:
+        #     with autocast('cuda'):
+        #         pred_real = netD(real)
+        #         pred_fake = netD(fake.detach())
+        #         loss_D_real = self.criterionGAN(pred_real, True)
+        #         loss_D_fake = self.criterionGAN(pred_fake, False)
+        #         loss_D = (loss_D_real + loss_D_fake) * 0.5
+        #     self.scaler.scale(loss_D).backward()
+        # else:
+        #     pred_real = netD(real)
+        #     pred_fake = netD(fake.detach())
+        #     loss_D_real = self.criterionGAN(pred_real, True)
+        #     loss_D_fake = self.criterionGAN(pred_fake, False)
+        #     loss_D = (loss_D_real + loss_D_fake) * 0.5
+        #     loss_D.backward()
         return loss_D
 
     def backward_D_A(self):
@@ -210,22 +298,62 @@ class CycleGANStructModel(BaseModel):
             self.loss_struct = self.opt.lambda_struct * self.calculate_struct_loss(self.real_A_depth, self.fake_B)
         else:
             self.loss_struct = 0
+        
+        # Arthur: add temporal loss
+        if self.opt.lambda_temporal > 0 and self.prev_fake_B is not None:
+            self.loss_temporal = self.opt.lambda_temporal * torch.nn.functional.mse_loss(self.fake_B, self.prev_fake_B)  # compute loss between batches
+        else:
+            self.loss_temporal = 0
+        
+        # Arthur: add optical flow loss
+        if self.opt.lambda_flow > 0 and self.prev_real_A is not None:  # GAN tensor range [-1, 1]
+            flow_real_A = self.compute_raft_flow(self.prev_real_A, self.real_A)
+            flow_fake_B = self.compute_raft_flow(self.prev_fake_B, self.fake_B)
+            # # Swap argument order to get correct optical flow maps (don't know why)
+            # flow_real_A = self.compute_raft_flow(self.real_A, self.prev_real_A) 
+            # flow_fake_B = self.compute_raft_flow(self.fake_B, self.prev_fake_B)
+            # normalize
+            flow_real_A_norm = flow_real_A / (flow_real_A.abs().max() + 1e-8)
+            flow_fake_B_norm = flow_fake_B / (flow_fake_B.abs().max() + 1e-8)
+            # compute MSE
+            self.loss_flow = self.opt.lambda_flow * torch.nn.functional.mse_loss(flow_real_A_norm, flow_fake_B_norm)
+            # convert flow to RGB for visualization
+            self.flow_real_A_img = flow_to_image(flow_real_A).float() / 255.0
+            self.flow_fake_B_img = flow_to_image(flow_fake_B).float() / 255.0
+        else:
+            self.loss_flow = 0
 
         # GAN loss D_A(G_A(A))
         self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
         # GAN loss D_B(G_B(B))
         self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
+        # # Arthur: mixed precision wrapper
+        # if self.opt.amp:
+        #     with autocast('cuda'):
+        #         self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
+        #         self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
+        # else:
+        #     self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
+        #     self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
+
         # Forward cycle loss || G_B(G_A(A)) - A||
         self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * lambda_A
         # Backward cycle loss || G_A(G_B(B)) - B||
         self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * lambda_B
-        # combined loss and calculate gradients
-        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B + self.loss_struct
+        
+        # combine losses & calculate gradients
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B + self.loss_struct + self.loss_temporal + self.loss_flow
+
         if self.opt.amp:
             with amp.scale_loss(self.loss_G, self.optimizer_G) as scaled_loss:
                 scaled_loss.backward()
         else:
             self.loss_G.backward()
+        # # Arthur: mixed precision wrapper
+        # if self.opt.amp:
+        #     self.scaler.scale(self.loss_G).backward()
+        # else:
+        #     self.loss_G.backward()
 
     def data_dependent_initialize(self, data):
         return
@@ -245,18 +373,60 @@ class CycleGANStructModel(BaseModel):
     def optimize_parameters(self):
         """Calculate losses, gradients, and update network weights; called in every training iteration"""
         # forward
-        self.forward()      # compute fake images and reconstruction images.
+        self.forward()  # compute fake images & reconstruction images.
+        
         # G_A and G_B
         self.set_requires_grad([self.netD_A, self.netD_B], False)  # Ds require no gradients when optimizing Gs
         self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
         self.backward_G()             # calculate gradients for G_A and G_B
         self.optimizer_G.step()       # update G_A and G_B's weights
+        # # Arthur: mixed precision wrapper
+        # if self.opt.amp:
+        #     self.scaler.step(self.optimizer_G)
+        # else:
+        #     self.optimizer_G.step()
+
         # D_A and D_B
         self.set_requires_grad([self.netD_A, self.netD_B], True)
         self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
         self.backward_D_A()      # calculate gradients for D_A
         self.backward_D_B()      # calculate graidents for D_B
         self.optimizer_D.step()  # update D_A and D_B's weights
+        # # Arthur: mixed precision wrapper
+        # if self.opt.amp:
+        #     self.scaler.step(self.optimizer_D)
+        #     self.scaler.update()  
+        # else:
+        #     self.optimizer_D.step()
+
+        # if self.prev_real_A is not None:
+            # print(f"Previous path: {self.prev_image_path}")
+            # print(f"Current path:  {self.image_paths[0]}")
+
+        # Arthur: store frames used in temporal & flow losses
+        self.prev_real_A = self.real_A.detach().clone()
+        self.prev_fake_B = self.fake_B.detach().clone() 
+        self.prev_path = self.image_paths[0]  # Store path for debugging
+
+        self.prev_path = self.image_paths[0]
+         
+        # Arthur: log losses in tensorboard every 100 steps
+        if self.step % 100 == 0:
+            self.writer.add_scalar('Loss/D_A', self.loss_D_A, self.step)
+            self.writer.add_scalar('Loss/G_A', self.loss_G_A, self.step)
+            self.writer.add_scalar('Loss/D_B', self.loss_D_B, self.step)
+            self.writer.add_scalar('Loss/G_B', self.loss_G_B, self.step)
+            self.writer.add_scalar('Loss/cycle_A', self.loss_cycle_A, self.step)
+            self.writer.add_scalar('Loss/cycle_B', self.loss_cycle_B, self.step)
+            self.writer.add_scalar('Loss/idt_A', self.loss_idt_A, self.step)
+            self.writer.add_scalar('Loss/idt_B', self.loss_idt_B, self.step)
+            self.writer.add_scalar('Loss/struct', self.loss_struct, self.step)
+            if self.opt.lambda_temporal > 0:
+                self.writer.add_scalar('Loss/temporal', self.loss_temporal, self.step)
+            if self.opt.lambda_flow > 0:
+                self.writer.add_scalar('Loss/flow', self.loss_flow, self.step)
+        
+        self.step += 1
 
     def calculate_struct_loss(self, src_depth, tgt):
         consolidated_tgt = torch.mean(tgt, axis=-3)
